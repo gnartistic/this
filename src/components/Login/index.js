80d4cc262048ac6e14aa24b5adc0ed90a6ea0f69
charlie <gn4rtistic@gmail.com>
2022-11-13 22:08:02 -0600

    added validation to email input box, as well as more styling to login page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,33 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.scss'
 import Logo from '../../assets/images/rapptr.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+import { validateEmail } from '../../utils/helpers';
 
 const Login = () =>
 {
+    const [ formState, setFormState ] = useState( { email: '', password: '' } );
+
+    const [ errorMessage, setErrorMessage ] = useState( '' );
+    // const { email, password } = formState;
+
+    const handleChange = ( e ) =>
+    {
+        if( e.target.name === 'email' ) {
+            const isValid = validateEmail( e.target.value );
+
+            if( !isValid ) {
+                setErrorMessage( 'Not a valid email' )
+            } else {
+                    setErrorMessage( '' );
+            }
+        }
+        if( !errorMessage ) {
+            setFormState( { ...formState, [ e.target.name ]: e.target.value } );
+        }
+    };
+
+    // const handleSubmit = ( e ) =>
+    // {
+    //     e.preventDefault();
+    //     if( !errorMessage ) {
+    //         setFormState( { [ e.target.name ]: e.target.value } );
+    //         console.log( 'Form', formState );
+    //     }
+    // }
+
     return (
         <div className='login-container container'>
             <div className='login'>
                 {/* company name */}
-                <img className='logo-image' src={Logo} alt='rapptr logo' />
+                <h1 className='login-title'>
+                    <img className='logo-image' src={Logo} alt='rapptr logo' />
+                    <span className='labs'> labs</span>
+                </h1>
                 {/* container to contain the input elements with independent styling */}
                 <div className='input-container'>
+                {/* email input container */}
                     <div className='email'>
                         <h2 className='email-label'>Email</h2>
                         <div className='input-box'>
-                            <FontAwesomeIcon icon={faUser} color='#0b141d' />
-                            <input type='text' placeholder='user@rapptrlabs.com' />
+                            <FontAwesomeIcon icon={faUser} color='#3a8ebc' />
+                            <input type='email' name='email' onBlur={handleChange} placeholder='user@rapptrlabs.com' />
                         </div>
+                        {/* error message */}
+                        {errorMessage && (
+                            <p className="error-text">{errorMessage}</p>
+                        )}
                     </div>
+                    {/* password input container */}
                     <div className='password'>
                         <h2 className='password-label'>Password</h2>
                         <div className='input-box'>
-                            <FontAwesomeIcon icon={faLock} color='#0b141d' />
+                            <FontAwesomeIcon icon={faLock} color='#3a8ebc' />
                             <input type='password' placeholder='Must be at least 4 characters' />
-                            </div>
+                        </div>
                     </div>
-                    <button type='submit' className='login-button'>Login</button>
+                    <button data-testid="button" type='submit' className='login-button'>Login</button>
                 </div>
             </div>
         </div>
